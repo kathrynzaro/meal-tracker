5fb9f0b3b17abc9859675b61d8ea30e6257d80cc
kathrynzaro <zarokathryn@gmail.com>
2022-05-02 17:11:56 -0700

    ingredient event listener / object / push to array

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
-
+const form = document.getElementById('add-ingredient');
 // let state
-
+let ingredients = [];
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const itemData = new FormData(form);
+    let ingredient = {
+        ingredient: itemData.get('ingredient'), 
+        quantity: itemData.get('quantity'), 
+        measurement: itemData.get('measurement'),
+    };
+    ingredients.push(ingredient);
+    console.log(ingredient);
+});
